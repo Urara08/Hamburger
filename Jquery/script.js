a953efdf2653d0171915acc346f9b4421e40d500
githubUrara08 <github90606372+Urara08@users.noreply.github.com>
2021-11-20 15:44:15 +0900

    jsクラス作成前に修正

--- Jquery/script.js
+++ Jquery/script.js
@@ -1,22 +1,22 @@
 $(function(){
-window.onload = function(){
-    $('.toggle_btn').on('click', function () {//MENUボタンをクリックすると
-    $('.toggle_btn, .js-sidebar, .js-sidebar__menu__close-btn , .js-overlay').toggleClass('is-show',)//サイドバーが出る
-  });
+  window.onload = function(){
+      $('.toggle_btn').on('click', function () {//MENUボタンをクリックすると
+      $('.toggle_btn, .l-sidebar, .p-sidebar__menu__close-btn , .c-overlay').toggleClass('show',)//サイドバーが出る
+    });
 
-$('.js-sidebar__menu__close-btn').on('click', function () {//✕ボタンをクリックすると
-    $('.js-sidebar, .js-overlay').removeClass('is-show')//サイドバーが閉じる
-  });
+  $('.p-sidebar__menu__close-btn').on('click', function () {//✕ボタンをクリックすると
+      $('.l-sidebar, .c-overlay').removeClass('show')//サイドバーが閉じる
+    });
 
-$('input').focusin(function(e)  {//検索ボックスがフォーカスされると
-    $('.p-search-form__icon').addClass('active')//虫眼鏡が消える
-  });
+  $('input').focusin(function(e)  {//検索ボックスがフォーカスされると
+      $('.p-search-form__icon').addClass('active')//虫眼鏡が消える
+    });
 
-$('input').focusout(function(e)  {//検索ボックスのフォーカスが外れると
-    $('.p-search-form__icon').removeClass('active')//虫眼鏡が現れる
-  });
+  $('input').focusout(function(e)  {//検索ボックスのフォーカスが外れると
+      $('.p-search-form__icon').removeClass('active')//虫眼鏡が現れる
+    });
 
-$(window).on('resize', function () {//画面をリサイズすると
-    $('.js-sidebar, .js-overlay').removeClass('is-show')//サイドバーが閉じる
-  });
-}});
+  $(window).on('resize', function () {//画面をリサイズすると
+      $('.l-sidebar, .c-overlay').removeClass('show')//サイドバーが閉じる
+    });
+  }});
