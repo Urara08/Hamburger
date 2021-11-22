8a9dd6886c889dfe55146e14101a756f8b288caf
githubUrara08 <github90606372+Urara08@users.noreply.github.com>
2021-11-23 02:20:39 +0900

    検索BOXの虫眼鏡の有無のJ-Queryをjsクラスに変更

--- Jquery/script.js
+++ Jquery/script.js
@@ -8,14 +8,6 @@ $(function(){
       $('.l-sidebar, .c-overlay').removeClass('show')//サイドバーが閉じる
     });
 
-  $('input').focusin(function(e)  {//検索ボックスがフォーカスされると
-      $('.p-search-form__icon').addClass('active')//虫眼鏡が消える
-    });
-
-  $('input').focusout(function(e)  {//検索ボックスのフォーカスが外れると
-      $('.p-search-form__icon').removeClass('active')//虫眼鏡が現れる
-    });
-
   $(window).on('resize', function () {//画面をリサイズすると
       $('.l-sidebar, .c-overlay').removeClass('show')//サイドバーが閉じる
     });
