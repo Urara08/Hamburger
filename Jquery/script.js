07d11e89de6fd6347170022fda451e6d3fb99f23
githubUrara08 <github90606372+Urara08@users.noreply.github.com>
2021-11-23 11:07:44 +0900

    .show→.is-showに変更

--- Jquery/script.js
+++ Jquery/script.js
@@ -1,14 +1,14 @@
 $(function(){
   window.onload = function(){
       $('.toggle_btn').on('click', function () {//MENUボタンをクリックすると
-      $('.toggle_btn, .js-sidebar, .js-sidebar__menu__close-btn , .js-overlay').toggleClass('show',)//サイドバーが出る
+      $('.toggle_btn, .js-sidebar, .js-sidebar__menu__close-btn , .js-overlay').toggleClass('is-show',)//サイドバーが出る
     });
 
   $('.js-sidebar__menu__close-btn').on('click', function () {//✕ボタンをクリックすると
-      $('.js-sidebar, .js-overlay, .js-sidebar__menu__close-btn').removeClass('show')//サイドバーが閉じる
+      $('.js-sidebar, .js-overlay, .js-sidebar__menu__close-btn').removeClass('is-show')//サイドバーが閉じる
     });
 
   $(window).on('resize', function () {//画面をリサイズすると
-      $('.js-sidebar, .js-overlay').removeClass('show')//サイドバーが閉じる
+      $('.js-sidebar, .js-overlay, .js-sidebar__menu__close-btn').removeClass('is-show')//サイドバーが閉じる
     });
   }});
