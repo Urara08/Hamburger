47f0dfd8cb9426da07026385f43b9e70ff4e99b4
githubUrara08 <github90606372+Urara08@users.noreply.github.com>
2021-11-27 14:10:19 +0900

    pagenation　styleを実装（pc)

---
+++ Jquery/script_pagination.js
@@ -0,0 +1,10 @@
+<script type="text/javascript">
+    jQuery(document).ready(function($){
+        $('.list-group').paginathing({
+            perPage: 3,//1ページ３アイテム表示
+            firstLast: true,//最初と最後のボタンも表示する
+            prevText:'prev' ,
+            nextText:'next' ,
+        })
+    });
+</script>
